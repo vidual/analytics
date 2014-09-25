d829424e949d9ea620453652f462e0e86dc08268
rich-iannone <riannone@me.com>
2014-09-24 21:48:45 -0700

    Modify roxygen statements to reflect change in function

--- listing_CSV_to_JSON.R
+++ listing_CSV_to_JSON.R
@@ -1,6 +1,6 @@
-#' Transform a Vidual CSV file to JSON for visualization
-#' @description This function takes a specifically-formatted CSV file and outputs a JSON file for visualization.
-#' @param CSV_filename the filename for the CSV file (located in the working directory) to be JSONified
+#' Transform a Vidual input data to JSON for visualization
+#' @description This function takes a input parameters for a Vidual listing and outputs a JSON file for visualization.
+#' @param 
 #' @param JSON_filename the desired filename for the JSON output file 
 #' @export listing_CSV_to_JSON
 
