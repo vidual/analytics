bc69d2a799e2d46c6fabb25e404348d85646e8cf
rich-iannone <riannone@me.com>
2014-09-24 21:49:01 -0700

    Add arguments to function

--- listing_CSV_to_JSON.R
+++ listing_CSV_to_JSON.R
@@ -4,8 +4,39 @@
 #' @param JSON_filename the desired filename for the JSON output file 
 #' @export listing_CSV_to_JSON
 
-listing_CSV_to_JSON <- function(CSV_filename,
-                                JSON_filename){
+listing_data_to_JSON <- function(vimeo_youtube_URL = NULL,
+                                 images = NULL
+                                 property_type = NULL,
+                                 property_view = NULL,
+                                 architectural_style = NULL,
+                                 sale_or_rent = NULL,
+                                 rent_period = NULL,
+                                 listing_price = NULL,
+                                 listing_date = NULL,
+                                 beds = NULL,
+                                 baths_full = NULL,
+                                 baths_half = NULL,
+                                 baths_total = NULL,
+                                 neighborhood = NULL,
+                                 address_1 = NULL,
+                                 address_2 = NULL,
+                                 city = NULL,
+                                 county = NULL,
+                                 state_province = NULL,
+                                 country = NULL,
+                                 zip_postal = NULL,
+                                 mls_num = NULL,
+                                 lot_size = NULL,
+                                 square_feet = NULL,
+                                 garage_parking_spaces = NULL,
+                                 year_built = NULL,
+                                 lon = NULL,
+                                 lat = NULL,
+                                 short_description = NULL,
+                                 amenities){
+  
+  
+  
   
   
   
