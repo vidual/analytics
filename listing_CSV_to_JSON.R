46fa86697ec30588eec788a3af060205631c49c5
rich-iannone <riannone@me.com>
2014-09-24 21:36:40 -0700

    Add roxygen statements before function definition

--- listing_CSV_to_JSON.R
+++ listing_CSV_to_JSON.R
@@ -1,5 +1,12 @@
-listing_CSV_to_JSON <- function(CSV_file,
+#' Transform a Vidual CSV file to JSON for visualization
+#' @description This function takes a specifically-formatted CSV file and outputs a JSON file for visualization.
+#' @param CSV_filename the filename for the CSV file (located in the working directory) to be JSONified
+#' @param JSON_filename the desired filename for the JSON output file 
+#' @export listing_CSV_to_JSON
+
+listing_CSV_to_JSON <- function(CSV_filename,
                                 JSON_filename){
+  
   
   
 }
