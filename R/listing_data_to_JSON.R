690243a0f1e3570c8766d7d38806fc5444a9096f
rich-iannone <riannone@me.com>
2014-09-24 22:06:21 -0700

    Add roxygen @param values for all arguments

--- R/listing_data_to_JSON.R
+++ R/listing_data_to_JSON.R
@@ -1,7 +1,35 @@
 #' Transform a Vidual input data to JSON for visualization
 #' @description This function takes a input parameters for a Vidual listing and outputs a JSON file for visualization.
-#' @param 
-#' @param JSON_filename the desired filename for the JSON output file 
+#' @param vimeo_youtube_URL 
+#' @param images 
+#' @param property_type 
+#' @param property_view 
+#' @param architectural_style 
+#' @param sale_or_rent 
+#' @param rent_period 
+#' @param listing_price 
+#' @param listing_date 
+#' @param beds 
+#' @param baths_full 
+#' @param baths_half 
+#' @param baths_total 
+#' @param neighborhood 
+#' @param address_1 
+#' @param address_2 
+#' @param city 
+#' @param county 
+#' @param state_province 
+#' @param country 
+#' @param zip_postal 
+#' @param mls_num 
+#' @param lot_size 
+#' @param square_feet 
+#' @param garage_parking_spaces 
+#' @param year_built 
+#' @param lon 
+#' @param lat 
+#' @param short_description 
+#' @param amenities
 #' @export listing_CSV_to_JSON
 
 listing_data_to_JSON <- function(vimeo_youtube_URL = NULL,
